1cffe3974859a1716d4b2902ac2614879a24b445
Jamie Hewland <jhewland@gmail.com>
2017-11-23 10:12:02 +0200

    Rename gunicorn_volume -> socket_volume

--- seaworthy/fixtures.py
+++ seaworthy/fixtures.py
@@ -12,17 +12,17 @@ NGINX_IMAGE = pytest.config.getoption("--nginx-image")
 class DjangoContainer(ContainerDefinition):
     WAIT_PATTERNS = (r"Booting worker",)
 
-    def __init__(self, name, gunicorn_volume, static_volume, db_url,
+    def __init__(self, name, socket_volume, static_volume, db_url,
                  image=DJANGO_IMAGE):
         super().__init__(name, image, self.WAIT_PATTERNS)
-        self.gunicorn_volume = gunicorn_volume
+        self.socket_volume = socket_volume
         self.static_volume = static_volume
         self.db_url = db_url
 
     def base_kwargs(self):
         return {
             "volumes": {
-                self.gunicorn_volume.inner(): "/var/run/gunicorn",
+                self.socket_volume.inner(): "/var/run/gunicorn",
                 self.static_volume.inner(): "/app/static:ro",
             },
             "environment": {"DATABASE_URL": self.db_url}
@@ -65,33 +65,33 @@ class NginxContainer(ContainerDefinition):
                     time.sleep(0.1)
 
 
-gunicorn_volume = VolumeDefinition("gunicorn")
-gunicorn_fixture = gunicorn_volume.pytest_fixture("gunicorn_volume")
+socket_volume = VolumeDefinition("socket")
+socket_volume_fixture = socket_volume.pytest_fixture("socket_volume")
 static_volume = VolumeDefinition("static")
-static_fixture = static_volume.pytest_fixture("static_volume")
+static_volume_fixture = static_volume.pytest_fixture("static_volume")
 
 
 postgresql_container = PostgreSQLContainer("postgresql")
 postgresql_fixture, clean_postgresql_fixture = (
     postgresql_container.pytest_clean_fixtures("postgresql_container"))
 
-django_container = DjangoContainer("django", gunicorn_volume, static_volume,
+django_container = DjangoContainer("django", socket_volume, static_volume,
                                    postgresql_container.database_url())
 django_fixture = django_container.pytest_fixture(
     "django_container",
-    dependencies=["gunicorn_volume", "static_volume", "postgresql_container"])
+    dependencies=["socket_volume", "static_volume", "postgresql_container"])
 
-nginx_container = NginxContainer("nginx", gunicorn_volume, static_volume)
+nginx_container = NginxContainer("nginx", socket_volume, static_volume)
 nginx_fixture = nginx_container.pytest_fixture(
     "nginx_container",
-    dependencies=["gunicorn_volume", "static_volume", "django_container"])
+    dependencies=["socket_volume", "static_volume", "django_container"])
 
 # Allow all the fixtures to be imported like `from fixtures import *`
 __all__ = [
     "clean_postgresql_fixture",
     "django_fixture",
-    "gunicorn_fixture",
     "nginx_fixture",
     "postgresql_fixture",
-    "static_fixture",
+    "socket_volume_fixture",
+    "static_volume_fixture",
 ]
