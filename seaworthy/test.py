35fd55be7e062c17a982e5499f16d44cbbafd6ee
sunho <graybat21@gmail.com>
2018-09-01 13:53:36 +0900

    Update test.py

--- seaworthy/test.py
+++ seaworthy/test.py
@@ -11,22 +11,22 @@ def mime_type(content_type):
     return cgi.parse_header(content_type)[0]
 
 
-class TestDjangoContainer:
-    @pytest.mark.clean_postgresql_container
-    def test_db_tables_created(self, django_container, postgresql_container):
-        """
-        When the Django container starts, it runs its migrations and some
-        database tables are created in PostgreSQL.
-        """
-        django_logs = django_container.get_logs().decode("utf-8")
-        assert "Running migrations" in django_logs
+# class TestDjangoContainer:
+#     @pytest.mark.clean_postgresql_container
+#     def test_db_tables_created(self, django_container, postgresql_container):
+#         """
+#         When the Django container starts, it runs its migrations and some
+#         database tables are created in PostgreSQL.
+#         """
+#         django_logs = django_container.get_logs().decode("utf-8")
+#         assert "Running migrations" in django_logs
 
-        psql_output = postgresql_container.exec_psql(
-            ("SELECT COUNT(*) FROM information_schema.tables WHERE "
-             "table_schema='public';"))
+#         psql_output = postgresql_container.exec_psql(
+#             ("SELECT COUNT(*) FROM information_schema.tables WHERE "
+#              "table_schema='public';"))
 
-        count = int(psql_output)
-        assert count > 0
+#         count = int(psql_output)
+#         assert count > 0
 
 
 class TestNginxContainer:
